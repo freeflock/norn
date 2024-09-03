a17fc158c9d7f747017bbe65871f163586866762
josiah <josiah@vagabondsystems.com>
2024-09-03 04:52:50 +0000

    (~)

--- packages/norn/main.py
+++ packages/norn/main.py
@@ -7,7 +7,7 @@ from loguru import logger
 from ratatosk_errands.adapter import Rabbit
 from ratatosk_errands.model import Errand, TextToImageInstructions, ImageToImageInstructions, ChatInstructions
 
-from norn.diffusion import StableDiffusion3TextToImage, StableDiffusion3ImageToImage
+from norn.diffusion import StableDiffusion3TextToImage, StableDiffusion3ImageToImage, FluxTextToImage
 from norn.language import Hermes3Chat
 
 RABBIT_HOST = os.getenv("RABBIT_HOST")
@@ -21,16 +21,17 @@ GALLERY_KEY = os.getenv("GALLERY_KEY")
 
 MODEL_TYPE = os.getenv("MODEL_TYPE")
 logger.info(f"( ) initializing model: {MODEL_TYPE}")
-diffusion_model: (StableDiffusion3TextToImage |
-                  StableDiffusion3ImageToImage |
-                  None) = None
-language_model: Hermes3Chat | None = None
-if MODEL_TYPE == "diffusion":
-    diffusion_model = StableDiffusion3TextToImage()
-    language_model = None
-elif MODEL_TYPE == "language":
-    diffusion_model = None
-    language_model = Hermes3Chat()
+model: (StableDiffusion3TextToImage |
+        StableDiffusion3ImageToImage |
+        FluxTextToImage |
+        Hermes3Chat |
+        None) = None
+if MODEL_TYPE == "text_to_image":
+    model = FluxTextToImage()
+if MODEL_TYPE == "image_to_image":
+    model = StableDiffusion3ImageToImage()
+elif MODEL_TYPE == "chat":
+    model = Hermes3Chat()
 else:
     raise ValueError(f"invalid model type specified in MODEL_TYPE env var: {MODEL_TYPE}")
 logger.info(f"(*) initialized model")
@@ -58,43 +59,43 @@ def download_image_from_gallery(identifier: str) -> Image:
     return image
 
 
-def receive_language_errand(ch, method, properties, body):
+def receive_chat_errand(ch, method, properties, body):
     try:
         logger.info(f"( ) starting errand: {body}")
         errand = Errand.model_validate_json(body)
         if not isinstance(errand.instructions, ChatInstructions):
             raise ValueError(f"unknown errand instructions on errand: {errand}")
-        response = language_model.chat(errand.instructions)
+        response = model.chat(errand.instructions)
         logger.info(response)
         logger.info(f"(*) completed errand: {errand.identifier}")
     except Exception as error:
         logger.error(f"(!) errand failed with error: {error}")
 
 
-def receive_diffusion_errand(ch, method, properties, body):
-    global diffusion_model
+def receive_text_to_image_errand(ch, method, properties, body):
     try:
         logger.info(f"( ) starting errand: {body}")
         errand = Errand.model_validate_json(body)
-        if isinstance(errand.instructions, TextToImageInstructions):
-            if not isinstance(diffusion_model, StableDiffusion3TextToImage):
-                logger.info(f"( ) initializing stable diffusion 3 text to image model")
-                diffusion_model = StableDiffusion3TextToImage()
-                logger.info(f"(*) initialized stable diffusion 3 text to image model")
-            image = diffusion_model.text_to_image(errand.instructions)
-            logger.info(f"uploading image")
-            upload_image_to_gallery(errand.identifier, image)
-        elif isinstance(errand.instructions, ImageToImageInstructions):
-            if not isinstance(diffusion_model, StableDiffusion3ImageToImage):
-                logger.info(f"( ) initializing stable diffusion 3 image to image model")
-                diffusion_model = StableDiffusion3ImageToImage()
-                logger.info(f"(*) initialized stable diffusion 3 image to image model")
-            base_image = download_image_from_gallery(errand.instructions.base_image_identifier)
-            image = diffusion_model.image_to_image(errand.instructions, base_image)
-            logger.info(f"uploading image")
-            upload_image_to_gallery(errand.identifier, image)
-        else:
+        if not isinstance(errand.instructions, TextToImageInstructions):
             raise ValueError(f"unknown errand instructions on errand: {errand}")
+        image = model.text_to_image(errand.instructions)
+        logger.info(f"uploading image")
+        upload_image_to_gallery(errand.identifier, image)
+        logger.info(f"(*) completed errand: {errand.identifier}")
+    except Exception as error:
+        logger.error(f"(!) errand failed with error: {error}")
+
+
+def receive_image_to_image_errand(ch, method, properties, body):
+    try:
+        logger.info(f"( ) starting errand: {body}")
+        errand = Errand.model_validate_json(body)
+        if not isinstance(errand.instructions, ImageToImageInstructions):
+            raise ValueError(f"unknown errand instructions on errand: {errand}")
+        base_image = download_image_from_gallery(errand.instructions.base_image_identifier)
+        image = model.image_to_image(errand.instructions, base_image)
+        logger.info(f"uploading image")
+        upload_image_to_gallery(errand.identifier, image)
         logger.info(f"(*) completed errand: {errand.identifier}")
     except Exception as error:
         logger.error(f"(!) errand failed with error: {error}")
@@ -102,16 +103,21 @@ def receive_diffusion_errand(ch, method, properties, body):
 
 def main():
     with Rabbit(RABBIT_HOST, RABBIT_PORT, RABBIT_USERNAME, RABBIT_PASSWORD) as rabbit:
-        if MODEL_TYPE == "diffusion":
-            rabbit.channel.queue_declare(queue="diffusion")
-            rabbit.channel.basic_consume(queue="diffusion",
+        if MODEL_TYPE == "text_to_image":
+            rabbit.channel.queue_declare(queue="text_to_image")
+            rabbit.channel.basic_consume(queue="text_to_image",
                                          auto_ack=True,
-                                         on_message_callback=receive_diffusion_errand)
-        elif MODEL_TYPE == "language":
-            rabbit.channel.queue_declare(queue="language")
-            rabbit.channel.basic_consume(queue="language",
+                                         on_message_callback=receive_text_to_image_errand)
+        if MODEL_TYPE == "image_to_image":
+            rabbit.channel.queue_declare(queue="image_to_image")
+            rabbit.channel.basic_consume(queue="image_to_image",
                                          auto_ack=True,
-                                         on_message_callback=receive_language_errand)
+                                         on_message_callback=receive_image_to_image_errand)
+        elif MODEL_TYPE == "chat":
+            rabbit.channel.queue_declare(queue="chat")
+            rabbit.channel.basic_consume(queue="chat",
+                                         auto_ack=True,
+                                         on_message_callback=receive_chat_errand)
         else:
             raise ValueError(f"invalid model type specified in MODEL_TYPE env var: {MODEL_TYPE}")
         logger.info(f"setup complete, listening for errands")
