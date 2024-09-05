6bc1b35e84bf56ed3fb32cb2b8411b90fcb006d3
josiah <josiah@vagabondsystems.com>
2024-09-05 04:20:28 +0000

    (~)

--- packages/norn/main.py
+++ packages/norn/main.py
@@ -5,7 +5,8 @@ import requests
 from PIL import Image
 from loguru import logger
 from ratatosk_errands.adapter import Rabbit
-from ratatosk_errands.model import Errand, TextToImageInstructions, ImageToImageInstructions, ChatInstructions
+from ratatosk_errands.model import Errand, Echo, TextToImageInstructions, ImageToImageInstructions, ChatInstructions, \
+    DiffusionReply, ChatReply
 
 from norn.diffusion import StableDiffusion3TextToImage, StableDiffusion3ImageToImage, FluxTextToImage
 from norn.language import Hermes3Chat
@@ -59,43 +60,73 @@ def download_image_from_gallery(identifier: str) -> Image:
     return image
 
 
-def receive_chat_errand(ch, method, properties, body):
+def receive_chat_errand(channel, method, properties, body):
     try:
-        logger.info(f"( ) starting errand: {body}")
+        logger.info(f"( ) receiving errand: {body}")
         errand = Errand.model_validate_json(body)
         if not isinstance(errand.instructions, ChatInstructions):
             raise ValueError(f"unknown errand instructions on errand: {errand}")
-        response = model.chat(errand.instructions)
-        logger.info(response)
+
+        logger.info(f"running inference")
+        reply_message = model.chat(errand.instructions)
+
+        logger.info(f"emitting echo")
+        reply = ChatReply(message=reply_message)
+        echo = Echo(errand=errand, reply=reply)
+        channel.basic_publish(exchange="", routing_key="echo", body=echo.model_dump_json())
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
         logger.info(f"(*) completed errand: {errand.identifier}")
     except Exception as error:
         logger.error(f"(!) errand failed with error: {error}")
 
 
-def receive_text_to_image_errand(ch, method, properties, body):
+def receive_text_to_image_errand(channel, method, properties, body):
     try:
-        logger.info(f"( ) starting errand: {body}")
+        logger.info(f"( ) receiving errand: {body}")
         errand = Errand.model_validate_json(body)
         if not isinstance(errand.instructions, TextToImageInstructions):
             raise ValueError(f"unknown errand instructions on errand: {errand}")
+
+        logger.info(f"running inference")
         image = model.text_to_image(errand.instructions)
+
         logger.info(f"uploading image")
-        upload_image_to_gallery(errand.identifier, image)
+        upload_image_to_gallery(errand.instructions.image_identifier, image)
+
+        logger.info(f"emitting echo")
+        reply = DiffusionReply(image_identifier=errand.instructions.image_identifier)
+        echo = Echo(errand=errand, reply=reply)
+        channel.basic_publish(exchange="", routing_key="echo", body=echo.model_dump_json())
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
         logger.info(f"(*) completed errand: {errand.identifier}")
     except Exception as error:
         logger.error(f"(!) errand failed with error: {error}")
 
 
-def receive_image_to_image_errand(ch, method, properties, body):
+def receive_image_to_image_errand(channel, method, properties, body):
     try:
-        logger.info(f"( ) starting errand: {body}")
+        logger.info(f"( ) receiving errand: {body}")
         errand = Errand.model_validate_json(body)
         if not isinstance(errand.instructions, ImageToImageInstructions):
             raise ValueError(f"unknown errand instructions on errand: {errand}")
+
+        logger.info(f"downloading base image")
         base_image = download_image_from_gallery(errand.instructions.base_image_identifier)
+
+        logger.info(f"running inference")
         image = model.image_to_image(errand.instructions, base_image)
+
         logger.info(f"uploading image")
-        upload_image_to_gallery(errand.identifier, image)
+        upload_image_to_gallery(errand.instructions.image_identifier, image)
+
+        logger.info(f"emitting echo")
+        reply = DiffusionReply(image_identifier=errand.instructions.image_identifier)
+        echo = Echo(errand=errand, reply=reply)
+        channel.basic_publish(exchange="", routing_key="echo", body=echo.model_dump_json())
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
         logger.info(f"(*) completed errand: {errand.identifier}")
     except Exception as error:
         logger.error(f"(!) errand failed with error: {error}")
@@ -103,21 +134,17 @@ def receive_image_to_image_errand(ch, method, properties, body):
 
 def main():
     with Rabbit(RABBIT_HOST, RABBIT_PORT, RABBIT_USERNAME, RABBIT_PASSWORD) as rabbit:
+        rabbit.channel.basic_qos(prefetch_count=1)
+        rabbit.channel.queue_declare(queue="echo")
+        rabbit.channel.queue_declare(queue="text_to_image")
+        rabbit.channel.queue_declare(queue="image_to_image")
+        rabbit.channel.queue_declare(queue="chat")
         if MODEL_TYPE == "text_to_image":
-            rabbit.channel.queue_declare(queue="text_to_image")
-            rabbit.channel.basic_consume(queue="text_to_image",
-                                         auto_ack=True,
-                                         on_message_callback=receive_text_to_image_errand)
+            rabbit.channel.basic_consume(queue="text_to_image", on_message_callback=receive_text_to_image_errand)
         elif MODEL_TYPE == "image_to_image":
-            rabbit.channel.queue_declare(queue="image_to_image")
-            rabbit.channel.basic_consume(queue="image_to_image",
-                                         auto_ack=True,
-                                         on_message_callback=receive_image_to_image_errand)
+            rabbit.channel.basic_consume(queue="image_to_image", on_message_callback=receive_image_to_image_errand)
         elif MODEL_TYPE == "chat":
-            rabbit.channel.queue_declare(queue="chat")
-            rabbit.channel.basic_consume(queue="chat",
-                                         auto_ack=True,
-                                         on_message_callback=receive_chat_errand)
+            rabbit.channel.basic_consume(queue="chat", on_message_callback=receive_chat_errand)
         else:
             raise ValueError(f"invalid model type specified in MODEL_TYPE env var: {MODEL_TYPE}")
         logger.info(f"setup complete, listening for errands")
