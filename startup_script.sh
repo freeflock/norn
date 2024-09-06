a672f805c0e2f87e058b658bcb7764a58db1f762
josiah <josiah.chapman@gmail.com>
2024-09-06 19:16:43 -0400

    (~)

--- startup_script.sh
+++ startup_script.sh
@@ -14,9 +14,10 @@ if [ ! -f "$INITIAL_BOOT_INDICATOR" ]; then
     sudo adduser --disabled-password --gecos "" --shell /bin/bash --home /home/norn norn
     sudo usermod -a -G sudo norn
     sudo echo "norn ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopassword
-    sudo -u norn mkdir -p /home/norn/.ssh
+    sudo -u norn /bin/bash -c "mkdir -p /home/norn/.ssh"
     sudo cp /root/.ssh/authorized_keys /home/norn/.ssh/authorized_keys
     sudo chown norn:norn /home/norn/.ssh/authorized_keys
+    sudo -u norn /bin/bash -c "ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''"
 
     # security
     sudo apt -y install fail2ban
@@ -38,6 +39,15 @@ if [ ! -f "$INITIAL_BOOT_INDICATOR" ]; then
 }
 }
 EOF
+
+    # norn
+    sudo -u norn /bin/bash -c "git clone https://github.com/freeflock/norn.git ~/norn"
+    sudo -u norn /bin/bash -c "cd ~/norn && python3.11 -m venv .venv"
+    sudo -u norn /bin/bash -c "cd ~/norn && source .venv/bin/activate && pip install -r requirements.txt"
+    sudo -u norn /bin/bash -c "cd ~/norn && source .venv/bin/activate && pip install -r requirements-no-build-isolation.txt --no-build-isolation"
+
+    sudo -u norn /bin/bash -c "git clone https://github.com/freeflock/gallery.git ~/gallery"
+    sudo -u norn /bin/bash -c "git clone https://github.com/freeflock/ratatosk.git ~/ratatosk"
 
     sudo touch "$INITIAL_BOOT_INDICATOR"
     sudo reboot
