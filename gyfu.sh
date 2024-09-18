efa0bcd5a28367f72f5ccd417fc45a5348462918
josiah <josiah.chapman@gmail.com>
2024-09-18 00:22:19 -0400

    (~)

--- gyfu.sh
+++ gyfu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # requires a docker-container buildx driver
-docker buildx build --platform linux/arm64/v8,linux/amd64 --push -t josiahdc/norn:0.1 .
+docker buildx build --platform linux/arm64/v8 --push -t josiahdc/norn:0.1 .
